520af7e6394e869dad85fdcbafa5da02b05d8b22
chavda amit <chavdaamit1011@gmail.com>
2026-09-26 17:22:59 +0530

    feat(register): fix import statement for Ionicons in registration component feat(api): add new route for checking authentication status

--- guard-app/app/register.tsx
+++ guard-app/app/register.tsx
@@ -1,4 +1,4 @@
-import { Ionicons } from '@expo/vector-icons';
+111import { Ionicons } from '@expo/vector-icons';
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import * as ImageManipulator from 'expo-image-manipulator';
 import { useLocalSearchParams, useRouter } from 'expo-router';
